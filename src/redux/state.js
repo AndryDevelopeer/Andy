4728f759e1419c12e43f2e9a34e4626408afe3dc
AndryDevelopeer <malogorya@mail.ru>
2021-03-02 20:52:25 +0500

    Update state.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -202,4 +202,4 @@ export const newPostTextActionCreator = (text) => ({ type: ADD_TEXT_POST, text: 
 window.store = store;
 export default store;
 
-// from xubuntu 02.03.2021
+// from xubuntu 02.03.2021 v2
